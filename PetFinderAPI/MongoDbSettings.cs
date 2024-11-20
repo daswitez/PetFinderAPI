9654567e7ca1dc755e8c90fd7a7974066c84dc69
T402-18 <T402-18@DESKTOP-D9IF75B>
2024-11-20 11:24:43 -0400

    configuracion de mongo desde cloud

--- PetFinderAPI/MongoDbSettings.cs
+++ PetFinderAPI/MongoDbSettings.cs
@@ -3,7 +3,6 @@
     public class MongoDbSettings
     {
         public string ConnectionString { get; set; } = null!;
-        public string DatabaseName { get; set; } = null!;
     }
 
 }
--- PetFinderAPI/Program.cs
+++ PetFinderAPI/Program.cs
@@ -10,21 +10,30 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSingleton<IMongoClient>(serviceProvider =>
 {
     var connectionString = builder.Configuration.GetValue<string>("MongoDbSettings:ConnectionString");
+
+    // Validar que la cadena de conexión no esté vacía
     if (string.IsNullOrEmpty(connectionString))
     {
         throw new ArgumentNullException("MongoDb connection string is null or empty.");
     }
+
+    // Crear y devolver el cliente de MongoDB
     return new MongoClient(connectionString);
 });
 
+// Obtener la base de datos desde el cliente de MongoDB
 builder.Services.AddScoped(serviceProvider =>
 {
     var client = serviceProvider.GetRequiredService<IMongoClient>();
-    var databaseName = builder.Configuration.GetValue<string>("MongoDbSettings:DatabaseName");
+
+    // Extraer la base de datos de la cadena de conexión
+    var databaseName = new MongoUrl(builder.Configuration.GetValue<string>("MongoDbSettings:ConnectionString")).DatabaseName;
+
     if (string.IsNullOrEmpty(databaseName))
     {
         throw new ArgumentNullException("MongoDb database name is null or empty.");
     }
+
     return client.GetDatabase(databaseName);
 });
 
